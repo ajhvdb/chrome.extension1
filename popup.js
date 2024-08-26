a8ed270e45af8d34ff38a08651b5189137cdde78
erik <ajhvdb@gmail.com>
2024-08-26 23:57:00 +0200

    met opties

--- popup.js
+++ popup.js
@@ -1,1 +1,17 @@
 alert('Hello Popup!')
+
+
+//fetch if user sync is not enabled by adding "accountStatus" param
+//may need to be inside try block for legacy chrome users - otherwise it returns empy object for non-synced accounts
+// chrome.identity.getProfileUserInfo({'accountStatus': 'ANY'}, function(info) {
+//     email = info.email;
+//     console.log(info);
+//     document.querySelector('textarea').value=JSON.stringify(info);
+//   });
+  
+//   //If only want users with sync on you can user the code below
+//   chrome.identity.getProfileUserInfo(function(info) {
+//     email = info.email;
+//     console.log(info);
+//     document.querySelector('textarea').value=JSON.stringify(info);
+//   });
